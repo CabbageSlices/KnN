455cd9bcc748ee8369f927748507a506ebae1a6e
Nimesh Shrestha <nimesh.shrestha@hotmail.com>
2021-08-23 18:23:31 -0700

    setup eslint, husky, lint-staged

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,8 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import a from 'styles/Home.module.css'
 import styles from '../styles/Home.module.css'
 
-const Home: NextPage = () => {
+const Home = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -15,9 +13,9 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
+          <img src="https://a.ca" />
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
         <p className={styles.description}>
           Get started by editing <code className={styles.code}>pages/index.js</code>
         </p>
