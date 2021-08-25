a2b418c5e0fbc5e5d6263a7f244c44bd7037186c
Nimesh Shrestha <nimesh.shrestha@hotmail.com>
2021-08-24 20:35:56 -0700

    add styled components, donwgrade husky

---
+++ src/pages/test/index.jsx
@@ -0,0 +1,12 @@
+import Styled from 'styled-components'
+
+const Container = Styled.div`
+    background: red;
+    padding: 100px;
+    border: 1px solid black;
+    font-size: 40px;
+`
+
+const Test = () => <Container>Hi</Container>
+
+export default Test
